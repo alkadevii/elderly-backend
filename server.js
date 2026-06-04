09862f9ce20a584b9ab237f20fe4df02e79a5cfd
alkadevi <alkadevi012@gmail.com>
2026-06-04 19:17:28 +0530

    backend login,reg api

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Backend Connected Successfully!" });
+});
+
+app.use("/api/auth", require("./routes/authRoutes"));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
